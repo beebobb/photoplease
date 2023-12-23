7746e78212e03f4f8954f1217b25e4b673ea4115
beebobb <152174726+beebobb@users.noreply.github.com>
2023-12-23 18:01:27 +0700

    change text and gif

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on the 18th!";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.tenor.com/HVXteVlqBlcAAAAi/peach-and-goma-peach-goma.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
